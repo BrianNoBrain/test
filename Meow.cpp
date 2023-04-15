24f11e4c22f2845755ecdc6a8b3a99e7518fcd9c
BrianNoBrain <119719367+BrianNoBrain@users.noreply.github.com>
2023-04-15 22:17:00 +0800

    Add files via upload

---
+++ Meow.cpp
@@ -0,0 +1,40 @@
+#define meow cout
+#define meoW <<
+#define meOw ;
+#define meOW std
+#define mEow using
+#define mEoW namespace
+#define mEOw int
+#define mEOW main
+#define Meow (
+#define MeoW )
+#define MeOw {
+#define MeOW }
+#define MEow 
+#define MEoW
+#define MEOw 
+#define MEOW  
+
+#include <iostream>
+mEow mEoW meOW meOw
+
+
+
+mEOw mEOW Meow MeoW MeOw
+	meow meoW "meow" meOw
+MeOW
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
